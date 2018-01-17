a29df8238279e1bdf590b2100ce385eaba83ec98
David Anderson <dave@dubkat.com>
2018-01-17 14:11:50 -0700

    Add CPU and Memory samplers

---
+++ apm/samplers/cpu.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+from textwrap import dedent
+
+import psutil
+
+
+class Cpu(object):
+    def __init__(self):
+        self.last_run = datetime.now()
+        self.last_cpu_times = psutil.cpu_times()
+        self.num_processors = psutil.cpu_count()
+
+    def metric_type(self):
+        return "CPU"
+
+    def metric_name(self):
+        return "Utilization"
+
+    def human_name(self):
+        return "Process CPU"
+
+    def metrics(self):
+        """
+        TODO: after implementing metrics collector,
+        make sure this returns compatible metrics
+        """
+        return None
+
+    def run(self):
+        now = datetime.now()
+        cpu_times = psutil.cpu_times()
+
+        wall_clock_elapsed = now - self.last_run
+        if wall_clock_elapsed < 0:
+            self.save_times(now, cpu_times)
+            # TODO: these print lines need a proper logger
+            print(dedent(
+                         """
+                         {human_name}: Negative time elapsed.
+                         now: {now},
+                         last_run: {last_run},
+                         total time: {wall_clock_elapsed}.
+                         """
+                         )).format(human_name=self.human_name(),
+                                   now=now,
+                                   last_run=self.last_run)
+            return None
+
+        utime_elapsed = self.cpu_times.user - self.last_cpu_times.user
+        stime_elapsed = self.cpu_times.system - self.last_cpu_times.system
+        process_elapsed = utime_elapsed + stime_elapsed
+
+        # This can happen right after a fork.  This class starts up in
+        # pre-fork, records {u,s}time, then forks. This resets {u,s}time to 0
+        if process_elapsed < 0:
+            self.save_times(now, cpu_times)
+            print(dedent(
+                         """
+                         {human_name}: Negative process time elapsed.
+                         utime: {utime_elapsed},
+                         stime: {stime_elapsed},
+                         total time: {process_elapsed}.
+                         This is normal to see when starting a
+                         forking web server.
+                         """
+                        )).format(human_name=self.human_name(),
+                                  utime_elapsed=utime_elapsed,
+                                  stime_elapsed=stime_elapsed,
+                                  process_elapsed=process_elapsed)
+            return None
+
+        # Normalized to # of processors
+        normalized_wall_clock_elapsed = wall_clock_elapsed * self.num_processors
+
+        # If somehow we run for 0 seconds between calls, don't try to divide by 0
+        res = None
+        if normalized_wall_clock_elapsed == 0:
+            res = 0
+        else:
+            res = (process_elapsed / normalized_wall_clock_elapsed)*100
+
+        if res < 0:
+            self.save_times(now, cpu_times)
+            print(dedent(
+                         """
+                         {human_name}: Negative CPU.
+                         {process_elapsed} / {normalized_wall_clock_elapsed} * 100 ==> {res}
+                         """
+                         )).format(human_name=self.human_name(),
+                                   process_elapsed=process_elapsed,
+                                   normalized_wall_clock_elapsed=normalized_wall_clock_elapsed,
+                                   res=res)
+            return None
+
+        self.save_times(now, cpu_times)
+
+        print(dedent(
+                     """
+                     {human_name}: {res} [{num_processors} CPU(s)]
+                     """
+                     )).format(human_name=self.human_name(),
+                               res=res,
+                               num_processors=self.num_processors)
+
+        return res
+
+    def save_times(self, now, cpu_times):
+        self.last_run = now
+        self.cpu_times = cpu_times
---
+++ apm/samplers/memory.py
@@ -0,0 +1,41 @@
+import platform
+import resource
+
+
+class Memory(object):
+    # Account for Darwin returning maxrss in bytes and Linux in KB. Used by
+    # the slow converters. Doesn't feel like this should go here
+    # though...more of a utility.
+    @classmethod
+    def rss_to_mb(klass, rss):
+        kilobyte_adjust = 1024 if (platform.system == 'Darwin') else 1
+        return float(rss) / 1024 / kilobyte_adjust
+
+    @classmethod
+    def rss(klass):
+        return resource.getrusage(resource.RUSAGE_SELF).maxrss
+
+    @classmethod
+    def rss_in_mb(klass):
+        return klass.rss_to_mb(klass.rss())
+
+    def metric_type(self):
+        return "Memory"
+
+    def metric_name(self):
+        return "Physical"
+
+    def human_name(self):
+        return "Process Memory"
+
+    def metrics(self):
+        """
+        TODO: after implementing metrics collector,
+        make sure this returns compatible metrics
+        """
+        return None
+
+    def run(self):
+        res = self.__class__.rss_in_mb()
+        print("{human_name}: #{res}").format(human_name=self.human_name(),
+                                             res=res)
