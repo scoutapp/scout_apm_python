0e2786f197c65839ec4e2e1a17c28c61d4284794
David Anderson <dave@dubkat.com>
2018-01-29 13:41:41 -0700

    Add setup.py, update .gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scout_apm',
+      version='0.1',
+      description='Scout APM ',
+      url='https://github.com/scoutapp/scout_apm_python',
+      author='Scout',
+      author_email='support@scoutapp.com',
+      license='MIT',
+      packages=['scout_apm'],
+      zip_safe=False)
