bc2149a3c0e77a80870fded46369c42254989a9a
Chris Schneider <chris@scoutapp.com>
2018-02-06 10:55:08 -0700

    Add TagSpan and TagRequest commands

--- scout_apm/commands.py
+++ scout_apm/commands.py
@@ -1,5 +1,6 @@
 import json
 
+
 class StartSpan:
     def __init__(self, request_id, span_id, parent, operation):
         self.request_id = request_id
@@ -48,14 +49,46 @@ class FinishRequest:
         }}
 
 
+class TagSpan:
+    def __init__(self, request_id, span_id, tag, value):
+        self.request_id = request_id
+        self.span_id = span_id
+        self.tag = tag
+        self.value = value
+
+    def message(self):
+        return {'TagSpan': {
+            'request_id': self.request_id,
+            'span_id': self.span_id,
+            'tag': self.tag,
+            'value': self.value,
+        }}
+
+
+class TagRequest:
+    def __init__(self, request_id, tag, value):
+        self.request_id = request_id
+        self.tag = tag
+        self.value = value
+
+    def message(self):
+        return {'TagRequest': {
+            'request_id': self.request_id,
+            'tag': self.tag,
+            'value': self.value,
+        }}
+
+
 class CoreAgentVersion:
     def message(self):
         return {'CoreAgentVersion': {}}
+
 
 class CoreAgentVersionResponse:
     def __init__(self, message):
         self.loaded = json.loads(message)
         self.version = self.loaded['CoreAgentVersion']['version']
+
 
 class CoreAgentShutdown:
     def message(self):
