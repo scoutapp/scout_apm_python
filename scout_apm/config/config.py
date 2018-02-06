c440c02c5648ac1ea7d723d85befecc445acaad7
Chris Schneider <chris@scoutapp.com>
2018-02-06 10:58:32 -0700

    Log config at boot time

--- scout_apm/config/config.py
+++ scout_apm/config/config.py
@@ -3,11 +3,23 @@ from .yaml_file import YamlFile
 
 
 class ScoutConfig():
+    """
+    Configuration object for the ScoutApm agent.
+
+    Contains a list of configuration "layers". When a configuration key is
+    looked up, each layer is asked in turn if it knows the value. The first one
+    to answer affirmatively returns the value.
+
+    That means that Env overrides File overrides Default values.
+    """
     def __init__(self, config_file=None):
         # We have to ask the ENV configuration for where the config file even
         # *is*, so allow this to be created with or without a file
         if config_file is None:
-            self.layers = [ScoutConfigEnv(), ScoutConfigDefaults(), ScoutConfigNull()]
+            self.layers = [
+                ScoutConfigEnv(),
+                ScoutConfigDefaults(),
+                ScoutConfigNull()]
         else:
             self.layers = [
                 ScoutConfigEnv(),
@@ -15,15 +27,39 @@ class ScoutConfig():
                 ScoutConfigDefaults(),
                 ScoutConfigNull()]
 
-        print('Init ScoutConfig')
+        print('Configuration Loaded:')
+        self.log()
 
     def value(self, key):
+        return self.locate_layer_for_key(key).value(key)
+
+    def locate_layer_for_key(self, key):
         for layer in self.layers:
             if layer.has_key(key):
-                return layer.value(key)
+                return layer
 
+    def log(self):
+        for key in self.known_keys():
+            layer = self.locate_layer_for_key(key)
+            print('{:9}: {} = {}'.format(layer.name(), key, layer.value(key)))
+        print('')
+
+    def known_keys(self):
+        return [
+            'core_agent_socket',
+            'download_url',
+            'download_version',
+        ]
 
 class ScoutConfigEnv():
+    """
+    Reads configuration from environment by prefixing the key desired with SCOUT_
+    Example: the `log_level` config looks for SCOUT_LOG_LEVEL environment variable
+    """
+
+    def name(self):
+        return 'ENV'
+
     def has_key(self, key):
         env_key = self.modify_key(key)
         return env_key in os.environ
@@ -38,6 +74,13 @@ class ScoutConfigEnv():
 
 
 class ScoutConfigFile():
+    """
+    Reads configuration from a yaml file.
+    """
+
+    def name(self):
+        return 'File'
+
     def __init__(self, config_file='scout_apm.yml'):
         self.data = YamlFile(config_file).parse()
 
@@ -49,6 +92,13 @@ class ScoutConfigFile():
 
 
 class ScoutConfigDefaults():
+    """
+    Provides default values for important configurations
+    """
+
+    def name(self):
+        return 'Defaults'
+
     def __init__(self):
         self.defaults = {
                 'core_agent_socket': '/tmp/scout_core_agent',
@@ -65,6 +115,15 @@ class ScoutConfigDefaults():
 
 # Always returns None to any key
 class ScoutConfigNull():
+    """
+    Always answers that a key is present, but the value is None
+
+    Used as the last step of the layered configuration.
+    """
+
+    def name(self):
+        return 'Null'
+
     def has_key(self, key):
         return True
 
