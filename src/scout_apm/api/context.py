5b72ae392dca42c14717ef612154c0da832e220c
Chris Schneider <chris@scoutapp.com>
2018-04-03 16:36:34 -0600

    Add scout_apm.api.context module to record Request context

---
+++ src/scout_apm/api/context.py
@@ -0,0 +1,16 @@
+from scout_apm.core.tracked_request import TrackedRequest
+
+
+class Context():
+    @staticmethod
+    def add(key, value):
+        """Adds context to the currently executing request.
+
+        :key: Any String identifying the request context.
+              Example: "user_ip", "plan", "alert_count"
+        :value: Any json-serializable type.
+              Example: "1.1.1.1", "free", 100
+        :returns: nothing.
+        """
+        tr = TrackedRequest.instance()
+        tr.tag(key, value)
