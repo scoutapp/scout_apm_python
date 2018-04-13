1a64f2635076e3b99a1821806bf30ac3d809f386
Chris Schneider <chris@christopher-schneider.com>
2018-04-13 11:57:10 -0600

    Django middleware based instruments (#19)
    * Middleware to instrument View timings, instead of monkey patches
    
    * Install django middleware automatically when included as an App
    
    * Name middleware ScoutApmMiddleware
    
    * Add a MiddlewareTimingMiddleware
    
    * Remove usage of Django Signals in favor of Middleware

--- src/scout_apm/django/apps.py
+++ src/scout_apm/django/apps.py
@@ -1,42 +1,16 @@
 from __future__ import absolute_import
 
 from django.apps import AppConfig
-from django.core.signals import request_finished, request_started
 
 from scout_apm.django.instruments.sql import SQLInstrument
 from scout_apm.django.instruments.template import TemplateInstrument
-from scout_apm.django.instruments.view import ViewInstrument
 from scout_apm.django.config import ConfigAdapter
-from scout_apm.core.tracked_request import TrackedRequest
 import scout_apm.core
 
 import logging
 
 
 logger = logging.getLogger(__name__)
-
-
-class DjangoSignals:
-    @staticmethod
-    def install():
-        request_started.connect(DjangoSignals.start_tracked_request,
-                                dispatch_uid='request_started_scoutapm')
-        request_finished.connect(DjangoSignals.stop_tracked_request,
-                                 dispatch_uid='request_stopped_scoutapm')
-        logger.debug('Added Django Signals')
-
-    # sender: django.core.handlers.wsgi.WSGIHandler
-    # kwargs: 'environ' => { ENV Key => Env Value }
-    #         'signal' => <django.dispatch.dispatcher.Signal object at 0x10ed7c470>
-    def start_tracked_request(sender, **kwargs):
-        # TODO: This is a good spot to extract headers
-        operation = 'Django'
-        tr = TrackedRequest.instance()
-        tr.start_span(operation=operation)
-        tr.mark_real_request()
-
-    def stop_tracked_request(sender, **kwargs):
-        TrackedRequest.instance().stop_span()
 
 
 class ScoutApmDjangoConfig(AppConfig):
@@ -53,9 +27,25 @@ class ScoutApmDjangoConfig(AppConfig):
         if installed is False:
             return
 
+        self.install_middleware()
 
         # Setup Instruments
-        DjangoSignals.install()
         SQLInstrument.install()
         TemplateInstrument.install()
-        ViewInstrument.install()
+
+    def install_middleware(self):
+        """
+        Attempts to insert the ScoutApm middleware as the first middleware
+        (first on incoming requests, last on outgoing responses).
+        """
+        from django.conf import settings
+
+        if isinstance(settings.MIDDLEWARE, tuple):
+            settings.MIDDLEWARE = (
+                ('scout_apm.django.middleware.MiddlewareTimingMiddleware', ) +
+                settings.MIDDLEWARE +
+                ('scout_apm.django.middleware.ViewTimingMiddleware', ))
+        else:
+            settings.MIDDLEWARE.insert(0, 'scout_apm.django.middleware.MiddlewareTimingMiddleware')
+            settings.MIDDLEWARE.append('scout_apm.django.middleware.ViewTimingMiddleware')
+
---
+++ src/scout_apm/django/middleware.py
@@ -0,0 +1,80 @@
+import logging
+from scout_apm.core.tracked_request import TrackedRequest
+
+# Logging
+logger = logging.getLogger(__name__)
+
+
+class MiddlewareTimingMiddleware:
+    """
+    Insert as early into the Middleware stack as possible (outermost layers),
+    so that other middlewares called after can be timed.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        operation = 'Middleware'
+
+        TrackedRequest.instance().start_span(operation=operation)
+        response = self.get_response(request)
+        TrackedRequest.instance().stop_span()
+        return response
+
+class ViewTimingMiddleware:
+    """
+    Insert as deep into the middleware stack as possible, ideally wrapping no
+    other middleware. Designed to time the View itself
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """
+        Wrap a single incoming request with start and stop calls.
+        This will start timing, but relies on the process_view callback to
+        capture more details about what view was really called, and other
+        similar info.
+
+        If process_view isn't called, then the request will not
+        be recorded.  This can happen if a middleware further along the stack
+        doesn't call onward, and instead returns a response directly.
+        """
+
+        tr = TrackedRequest.instance()
+        tr.mark_real_request()
+
+        # This operation name won't be recorded unless changed later in
+        # process_view
+        operation = 'Unknown'
+        tr.start_span(operation=operation)
+        response = self.get_response(request)
+        tr.stop_span()
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        """
+        Capture details about the view_func that is about to execute
+        """
+
+        view_name = request.resolver_match._func_path
+        span = TrackedRequest.instance().current_span()
+        if span is not None:
+            span.operation = 'Controller/' + view_name
+            span.tag('path', request.path)
+            span.tag('remote_addr', request.META['REMOTE_ADDR'])
+
+    def process_exception(self, request, exception):
+        """
+        Mark this request as having errored out
+
+        Does not modify or catch or otherwise change the exception thrown
+        """
+        TrackedRequest.instance().tag('error', 'true')
+
+    def process_template_response(self, request, response):
+        """
+        """
+        pass
