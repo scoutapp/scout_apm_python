d6332ebe9531f18b01164f5b1d3b148eda723a9d
Chris Schneider <chris@scoutapp.com>
2018-05-18 15:14:14 -0600

    Accomodate 3.4 and 2.7 versions of traceback module

--- src/scout_apm/core/traceback.py
+++ src/scout_apm/core/traceback.py
@@ -7,10 +7,19 @@ def capture():
     stack = traceback.extract_stack()
     formatted_stack = []
     for frame in stack:
+        if type(frame) is tuple:
+            filename = frame[0]
+            line = frame[1]
+            function = frame[3]
+        else:
+            filename = frame.filename
+            line = frame.lineno
+            function = frame.name
+
         formatted_stack.append({
-            'file': frame.filename,
-            'line': frame.lineno,
-            'function': frame.name,
+            'file': filename,
+            'line': line,
+            'function': function,
         })
 
     # Python puts the closest stack frames at the end of the traceback. But we
