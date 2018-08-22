48b78666af12a5fe64944a29fc6728256dd898b7
David Anderson <dave@dubkat.com>
2018-08-22 09:58:01 -0600

    Split up wheel building by travis containers

--- ci/cibuildwheel.sh
+++ ci/cibuildwheel.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
+#
+# This script is executed as the cibuildwheel test command.
+# It sets up the necessary environment and then runs tox.
+# Docs: https://github.com/joerick/cibuildwheel
+#
 
+# Exit with nonzero status if anything in this script errors.
 set -e
 
-export TOX_CONFIG="/project/tox.ini"
-export PY_VER=`echo $PATH | awk -F\- '{print $1}' | awk -F\/ '{print $4}' | tr 'cp' 'py'`
-export TOXENV=`tox -l -c ${TOX_CONFIG} | grep ${PY_VER}`
+# This should list all of the versions cibuildwheel supports.
+ALL_CIBW_VERSIONS="cp27-* cp33-* cp34-* cp35-* cp36-* cp37-*"
 
-echo "PY_VER: ${PY_VER}"
+# We want to skip all versions EXCEPT the one specified by travis in $CIBW_PY_VER
+export CIBW_SKIP=`echo ${ALL_CIBW_VERSIONS} | perl -pe "s/cp${CIBW_PY_VER}.*? //"`
+
+# cibuildwheel mounts the project directory into the docker container at /project
+export TOX_CONFIG="/project/tox.ini"
+
+# Only run the tox tests for the CIBW_PY_VER python verison.
+export TOXENV=`tox -l -c ${TOX_CONFIG} | grep "py${CIBW_PY_VER}"`
+
+echo ""
+echo "---------------- CIBW ENVIRONMENT VARIABLES -------------------"
+echo ""
+echo "CIBW_PY_VER: ${CIBW_PY_VER}"
+echo "CIBW_SKIP: ${CIBW_SKIP}"
 echo "TOX_CONFIG: ${TOX_CONFIG}"
 echo "TOXENV: ${TOXENV}"
+echo ""
+echo ""
 
 tox -v -c ${TOX_CONFIG}
