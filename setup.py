24fc736779020a1358a016adb09f5b9dd35319af
Aymeric Augustin <aymeric.augustin@m4x.org>
2018-11-26 17:30:55 +0100

    Improve and simplify Travis CI setup. (#135)
    * Run tests against built wheels.
    
    Previously, CIBW_TEST_COMMAND called tox, which created new virtualenvs
    for running test. This defeated the purpose of CIBW_TEST_COMMAND, namely
    testing with the result of installing the freshly-built wheel.
    
    Pick the version of Djang that exercises the most code paths. It doesn't
    seem worthwhile to re-run tests against multiple versions of Djano here.
    
    * Refactor conditions that are always true.
    
    * Move definition of env variables to global section.
    
    This removes a difference with the recommended configuration of
    cibuildwheel.
    
    Also remove CIBW_BUILD_VERBOSITY which was set to its default value.
    
    * Configure according to the cibuildwheel docs.
    
    * Keep the default verbosity of py.test.
    
    This avoids a massive output when running tox.
    
    * Run tox on all supported Python versions.
    
    * Introduce a Python version matrix so that tests run on
      all supported Python version in parallel.
    * Separate "test" and "build" stages so that builds run only
      when tests pass on all Python versions.
    
    * Declare support for Python 2.7 in setup.py.
    
    This isn't true yet but makes it possible to run tests with tox.
    
    Remove __future__ imports from setup.py because setuptools dislikes
    unicode and none of the other features are useful in this script.
    
    * Run tests with Python 2.7 on Travis CI.
    
    * Run style checks on Travis CI.
    
    * Use the default target locations.
    
    * Remove unnecessary entry in MANIFEST.in.

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import os
 import sys
 from glob import glob
@@ -27,7 +25,7 @@ setup_args = {
     "author_email": "support@scoutapp.com",
     "license": "MIT",
     "zip_safe": False,
-    "python_requires": ">=3.4, <4",
+    "python_requires": ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
     "packages": find_packages("src"),
     "package_dir": {"": "src"},
     "py_modules": [os.splitext(os.basename(path))[0] for path in glob("src/*.py")],
@@ -49,6 +47,8 @@ setup_args = {
         "Operating System :: MacOS",
         "Operating System :: POSIX",
         "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
