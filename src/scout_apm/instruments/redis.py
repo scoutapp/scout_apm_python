6cbee0c6b61fd9e724d3d9d7ada5cc80477a8669
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2018-11-28 08:19:02 +0100

    Add compatibility with redis-py 3.
    This accounts for breaking changes in this commit:
    
        https://github.com/andymccurdy/redis-py/commit/fad791c894c5d52921b4e449e1ba05133de5b2ec

--- src/scout_apm/instruments/redis.py
+++ src/scout_apm/instruments/redis.py
@@ -8,14 +8,26 @@ from scout_apm.core.tracked_request import TrackedRequest
 logger = logging.getLogger(__name__)
 
 
+def import_Redis_and_Pipeline():
+    import redis  # noqa: F401
+
+    if redis.VERSION[0] >= 3:
+        from redis import Redis  # noqa: F401
+        from redis.client import Pipeline  # noqa: F401
+    else:
+        from redis import StrictRedis as Redis  # noqa: F401
+        from redis.client import BasePipeline as Pipeline  # noqa: F401
+
+    return Redis, Pipeline
+
+
 class Instrument(object):
     def __init__(self):
         self.installed = False
 
     def installable(self):
         try:
-            from redis import StrictRedis  # noqa: F401
-            from redis.client import BasePipeline  # noqa: F401
+            Redis, Pipeline = import_Redis_and_Pipeline()
         except ImportError:
             logger.info("Unable to import for Redis instruments")
             return False
@@ -31,17 +43,17 @@ class Instrument(object):
 
         self.installed = True
 
-        self.patch_strictredis()
-        self.patch_basepipeline()
+        self.patch_redis()
+        self.patch_pipeline()
 
         logger.info("Instrumented Redis")
         return True
 
-    def patch_strictredis(self):
+    def patch_redis(self):
         try:
-            from redis import StrictRedis
+            Redis, _Pipeline = import_Redis_and_Pipeline()
 
-            @monkeypatch_method(StrictRedis)
+            @monkeypatch_method(Redis)
             def execute_command(original, self, *args, **kwargs):
                 try:
                     op = args[0]
@@ -61,11 +73,11 @@ class Instrument(object):
                 "Unable to instrument for Redis StrictRedis.execute_command: %r", e
             )
 
-    def patch_basepipeline(self):
+    def patch_pipeline(self):
         try:
-            from redis.client import BasePipeline
+            _Redis, Pipeline = import_Redis_and_Pipeline()
 
-            @monkeypatch_method(BasePipeline)
+            @monkeypatch_method(Pipeline)
             def execute(original, self, *args, **kwargs):
                 tr = TrackedRequest.instance()
                 tr.start_span(operation="Redis/MULTI")
