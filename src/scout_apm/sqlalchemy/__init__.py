e3c5d4b16bad4d7711fb67cf86631afbe57736e5
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2018-12-09 17:39:33 +0100

    Move function definitions to module level.
    This is a small performance optimization: it avoids redefining the
    functions every time instrument_sqlalchemy() is called.
    
    It's also a readability optimization: it makes it clear that these
    functions don't close over the engine variable.

--- src/scout_apm/sqlalchemy/__init__.py
+++ src/scout_apm/sqlalchemy/__init__.py
@@ -5,24 +5,25 @@ from sqlalchemy import event
 from scout_apm.core.tracked_request import TrackedRequest
 
 
+def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+    tr = TrackedRequest.instance()
+    span = tr.start_span(operation="SQL/Query")
+    span.tag("db.statement", statement)
+
+
+def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
+    tr = TrackedRequest.instance()
+    span = tr.current_span()
+    if span is not None:
+        tr.callset.update(statement, 1, span.duration())
+        if tr.callset.should_capture_backtrace(statement):
+            span.capture_backtrace()
+    tr.stop_span()
+
+
 def instrument_sqlalchemy(engine):
-    def before_cursor_execute(
-        conn, cursor, statement, parameters, context, executemany
-    ):
-        tr = TrackedRequest.instance()
-        span = tr.start_span(operation="SQL/Query")
-        span.tag("db.statement", statement)
-
-    def after_cursor_execute(conn, cursor, statement, parameters, context, executemany):
-        tr = TrackedRequest.instance()
-        span = tr.current_span()
-        if span is not None:
-            tr.callset.update(statement, 1, span.duration())
-            if tr.callset.should_capture_backtrace(statement):
-                span.capture_backtrace()
-        tr.stop_span()
-
-    if not getattr(engine, "_scout_instrumented", False):
-        event.listen(engine, "before_cursor_execute", before_cursor_execute)
-        event.listen(engine, "after_cursor_execute", after_cursor_execute)
-        setattr(engine, "_scout_instrumented", True)
+    if getattr(engine, "_scout_instrumented", False):
+        return
+    event.listen(engine, "before_cursor_execute", before_cursor_execute)
+    event.listen(engine, "after_cursor_execute", after_cursor_execute)
+    engine._scout_instrumented = True
