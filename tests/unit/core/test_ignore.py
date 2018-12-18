582f2fb5792f2b747e2e249cec9d445b50843fec
Chris Schneider <chris@scoutapp.com>
2018-12-18 15:37:21 -0700

    Add another ignore_path test

--- tests/unit/core/test_ignore.py
+++ tests/unit/core/test_ignore.py
@@ -4,11 +4,23 @@ from scout_apm.core.config import ScoutConfig
 from scout_apm.core.ignore import ignore_path
 
 
-def test_loads_class_instrument():
+def test_ignore():
     ScoutConfig.set(ignore=["/health"])
 
     assert ignore_path("/health")
     assert ignore_path("/health/foo")
+    assert not ignore_path("/users")
+
+    ScoutConfig.reset_all()
+
+
+def test_ignore_multiple_prefixes():
+    ScoutConfig.set(ignore=["/health", "/api"])
+
+    assert ignore_path("/health")
+    assert ignore_path("/health/foo")
+    assert ignore_path("/api")
+    assert ignore_path("/api/foo")
     assert not ignore_path("/users")
 
     ScoutConfig.reset_all()
