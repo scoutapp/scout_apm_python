15d007a2d7b9d4ff5d159ebc5f876fd272eaeaad
Mengxuan Xia <xiamx2004@gmail.com>
2019-03-04 14:11:58 -0500

    tracking transaction request

--- src/scout_apm/api/__init__.py
+++ src/scout_apm/api/__init__.py
@@ -54,8 +54,8 @@ def install(*args, **kwargs):
     scout_apm.core.install(*args, **kwargs)
 
 
-def ignore_transaction():
-    TrackedRequest.instance().tag("ignore_transaction", True)
+def ignore_transaction(tr):
+    tr.tag("ignore_transaction", True)
 
 
 class instrument(ContextDecorator):
@@ -64,15 +64,14 @@ class instrument(ContextDecorator):
         self.tags = tags
 
     def __enter__(self):
-        tr = TrackedRequest.instance()
+        self.tr = TrackedRequest.instance()
         self.span = tr.start_span(operation=self.operation)
         for key, value in self.tags.items():
             self.tag(key, value)
         return self
 
     def __exit__(self, *exc):
-        tr = TrackedRequest.instance()
-        tr.stop_span()
+        self.tr.stop_span()
         return False
 
     def tag(self, key, value):
@@ -95,24 +94,23 @@ class Transaction(ContextDecorator):
     def start(cls, kind, name, tags={}):
         operation = text(kind) + "/" + text(name)
 
-        tr = TrackedRequest.instance()
+        self.tr = TrackedRequest.instance()
         tr.mark_real_request()
         span = tr.start_span(operation=operation)
         for key, value in tags.items():
             tr.tag(key, value)
-        return span
+        return tr
 
     @classmethod
     def stop(cls):
-        tr = TrackedRequest.instance()
-        tr.stop_span()
+        self.tr.stop_span()
         return True
 
     # __enter__ must be defined by child classes.
 
     # *exc is any exception raised. Ignore that
     def __exit__(self, *exc):
-        WebTransaction.stop()
+        Transaction.stop()
         return False
 
     def tag(self, key, value):
@@ -123,10 +121,10 @@ class Transaction(ContextDecorator):
 class WebTransaction(Transaction):
     @classmethod
     def start(cls, name, tags={}):
-        Transaction.start("Controller", text(name), tags)
+        return Transaction.start("Controller", text(name), tags)
 
     def __enter__(self):
-        Transaction.start("Controller", self.name, self.tags)
+        return Transaction.start("Controller", self.name, self.tags)
 
 
 class BackgroundTransaction(Transaction):
