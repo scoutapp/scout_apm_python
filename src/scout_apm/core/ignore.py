07237915149c143a3fd9eeda5b0b6f67599774ea
Adam Johnson <me@adamj.eu>
2019-05-08 17:27:21 +0100

    Enforce UTF-8 coding in all Python files
    This is needed for Python 2.7, which without a coding comment, defaults to the system encoding. This can cause problems in rare situations - and I already managed to write a test that broke on Python 2.7 (emoji).

--- src/scout_apm/core/ignore.py
+++ src/scout_apm/core/ignore.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from scout_apm.core.context import AgentContext
 
 
--- src/scout_apm/django/__init__.py
+++ src/scout_apm/django/__init__.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 default_app_config = "scout_apm.django.apps.ScoutApmDjangoConfig"
