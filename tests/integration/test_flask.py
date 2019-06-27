4e5d97023d526e8bdf286d0d70f77129261bd460
Adam Johnson <me@adamj.eu>
2019-06-27 16:58:45 +0100

    Improve Pyramid tests (#193)
    * Create a completely fresh Pyramid app for each test
    * Improve tests to assert on `tracked_requests` as for other integrations
    * Get test coverage to 100%
    * Tidy up Flask test assertions whilst I'm looking at the file

--- tests/integration/test_flask.py
+++ tests/integration/test_flask.py
@@ -57,8 +57,9 @@ def app_with_scout(config=None):
 def test_home(tracked_requests):
     with app_with_scout() as app:
         response = TestApp(app).get("/")
-        assert response.status_int == 200
 
+    assert response.status_int == 200
+    assert response.text == "Welcome home."
     assert len(tracked_requests) == 1
     assert len(tracked_requests[0].complete_spans) == 1
     span = tracked_requests[0].complete_spans[0]
@@ -102,8 +103,9 @@ def test_user_ip(headers, extra_environ, expected, tracked_requests):
 def test_hello(tracked_requests):
     with app_with_scout() as app:
         response = TestApp(app).get("/hello/")
-        assert response.status_int == 200
 
+    assert response.status_int == 200
+    assert response.text == "Hello World!"
     assert len(tracked_requests) == 1
     assert len(tracked_requests[0].complete_spans) == 1
     span = tracked_requests[0].complete_spans[0]
@@ -129,16 +131,16 @@ def test_hello_options(tracked_requests):
 def test_not_found(tracked_requests):
     with app_with_scout() as app:
         response = TestApp(app).get("/not-found/", expect_errors=True)
-        assert response.status_int == 404
 
+    assert response.status_int == 404
     assert tracked_requests == []
 
 
 def test_server_error(tracked_requests):
     with app_with_scout() as app:
         response = TestApp(app).get("/crash/", expect_errors=True)
-        assert response.status_int == 500
 
+    assert response.status_int == 500
     assert len(tracked_requests) == 1
     tracked_request = tracked_requests[0]
     assert tracked_request.tags["error"] == "true"
@@ -167,6 +169,6 @@ def test_no_monitor(tracked_requests):
     # With an empty config, "SCOUT_MONITOR" defaults to False.
     with app_with_scout({}) as app:
         response = TestApp(app).get("/hello/")
-        assert response.status_int == 200
 
+    assert response.status_int == 200
     assert tracked_requests == []
