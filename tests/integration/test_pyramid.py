e0101ed4409a00d3cc1df85de321af3ce4a0a41d
Adam Johnson <me@adamj.eu>
2019-06-27 16:35:21 +0100

    Improve Pyramid tests
    * Create a completely fresh Pyramid app for each test
    * Improve tests to assert on `tracked_requests` as for other integrations
    * Get test coverage to 100%
    * Tidy up Flask test assertions whilst I'm looking at the file

--- tests/integration/test_pyramid.py
+++ tests/integration/test_pyramid.py
@@ -4,12 +4,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from contextlib import contextmanager
 
 import pytest
+from pyramid.config import Configurator
+from pyramid.response import Response
 from webtest import TestApp
 
 from scout_apm.api import Config
 from tests.compat import mock
-
-from .pyramid_app import app_configurator
 
 
 @contextmanager
@@ -25,11 +25,28 @@ def app_with_scout(config=None):
     # Disable running the agent.
     config["SCOUT_CORE_AGENT_LAUNCH"] = False
 
-    # Setup according to https://docs.scoutapm.com/#pyramid
-    with app_configurator() as configurator:
+    def home(request):
+        return Response("Welcome home.")
+
+    def hello(request):
+        return Response("Hello World!")
+
+    def crash(request):
+        raise ValueError("BØØM!")  # non-ASCII
+
+    with Configurator() as configurator:
+        configurator.add_route("home", "/")
+        configurator.add_view(home, route_name="home", request_method="GET")
+        configurator.add_route("hello", "/hello/")
+        configurator.add_view(hello, route_name="hello")
+        configurator.add_route("crash", "/crash/")
+        configurator.add_view(crash, route_name="crash")
+
+        # Setup according to https://docs.scoutapm.com/#pyramid
         configurator.add_settings(**config)
         configurator.include("scout_apm.pyramid")
         app = configurator.make_wsgi_app()
+
     try:
         yield app
     finally:
@@ -37,36 +54,19 @@ def app_with_scout(config=None):
         Config.reset_all()
 
 
-def test_home():
+def test_home(tracked_requests):
     with app_with_scout() as app:
-        response = TestApp(app).get("/")
-        assert response.status_int == 200
+        response = TestApp(app).get("/", expect_errors=True)
 
-
-def test_hello():
-    with app_with_scout() as app:
-        response = TestApp(app).get("/hello/")
-        assert response.status_int == 200
-
-
-def test_not_found():
-    with app_with_scout() as app:
-        response = TestApp(app).get("/not-found/", expect_errors=True)
-        assert response.status_int == 404
-
-
-def test_server_error():
-    with app_with_scout() as app:
-        # Unlike most other frameworks, Pyramid doesn't catch all exceptions.
-        with pytest.raises(ValueError):
-            TestApp(app).get("/crash/", expect_errors=True)
-
-
-def test_no_monitor():
-    # With an empty config, "SCOUT_MONITOR" defaults to False.
-    with app_with_scout({}) as app:
-        response = TestApp(app).get("/hello/")
-        assert response.status_int == 200
+    assert response.status_int == 200
+    assert response.text == "Welcome home."
+    assert len(tracked_requests) == 1
+    tracked_request = tracked_requests[0]
+    assert tracked_request.tags["path"] == "/"
+    assert tracked_request.tags["user_ip"] is None
+    assert len(tracked_request.complete_spans) == 1
+    span = tracked_request.complete_spans[0]
+    assert span.operation == "Controller/home"
 
 
 @mock.patch(
@@ -74,7 +74,7 @@ def test_no_monitor():
     new_callable=mock.PropertyMock,
     side_effect=ValueError,
 )
-def test_remote_addr_exception(remote_addr):
+def test_user_ip_exception(remote_addr, tracked_requests):
     """
     Scout doesn't crash if pyramid.request.Request.remote_addr raises an exception.
 
@@ -85,4 +85,55 @@ def test_remote_addr_exception(remote_addr):
     """
     with app_with_scout() as app:
         response = TestApp(app).get("/hello/")
-        assert response.status_int == 200
+
+    assert response.status_int == 200
+    assert "user_ip" not in tracked_requests[0].tags
+
+
+def test_home_ignored(tracked_requests):
+    with app_with_scout({"SCOUT_MONITOR": True, "SCOUT_IGNORE": ["/"]}) as app:
+        response = TestApp(app).get("/")
+
+    assert response.status_int == 200
+    assert response.text == "Welcome home."
+    assert tracked_requests == []
+
+
+def test_hello(tracked_requests):
+    with app_with_scout() as app:
+        response = TestApp(app).get("/hello/")
+
+    assert response.status_int == 200
+    assert response.text == "Hello World!"
+    assert len(tracked_requests) == 1
+    assert len(tracked_requests[0].complete_spans) == 1
+    span = tracked_requests[0].complete_spans[0]
+    assert span.operation == "Controller/hello"
+    # assert span.tags["path"] == "/hello/"
+
+
+def test_not_found(tracked_requests):
+    with app_with_scout() as app:
+        response = TestApp(app).get("/not-found/", expect_errors=True)
+
+    assert response.status_int == 404
+    assert tracked_requests == []
+    # TODO: assert that tracked request is released
+
+
+def test_server_error(tracked_requests):
+    # Unlike most other frameworks, Pyramid doesn't catch all exceptions.
+    with app_with_scout() as app, pytest.raises(ValueError):
+        TestApp(app).get("/crash/", expect_errors=True)
+
+    assert tracked_requests == []
+    # TODO: assert that tracked request is released
+
+
+def test_no_monitor(tracked_requests):
+    # With an empty config, "SCOUT_MONITOR" defaults to False.
+    with app_with_scout({}) as app:
+        response = TestApp(app).get("/hello/")
+
+    assert response.status_int == 200
+    assert tracked_requests == []
