scoutapp/scout_apm_python

commit 6ab579e22ab903bb6b72108bc485550bb151f353

AgentContext is a global, not thread-local
We want all AgentContext references to point to the same set of internal
data (configs for now, and whatever we add going down the line). Before,
it was a thread local, so each copy had a distinct instance of the
ScoutConfig class, which would make editing the config in the running
system much harder, and generally is surprising if you expect config to
be 'global'.

src/scout_apm/core/core_agent_manager.py
@@ -20,22 +20,22 @@ logger = logging.getLogger(__name__)
 
 
 class CoreAgentManager:
-    def __init__(self):
+    def __init__(self, context):
         self.core_agent_bin_path = None
         self.core_agent_bin_version = None
-        self.core_agent_dir = '{}/{}'.format(AgentContext.instance().config.value('core_agent_dir'),
-                                             AgentContext.instance().config.core_agent_full_name())
+        self.core_agent_dir = '{}/{}'.format(AgentContext.instance.config.value('core_agent_dir'),
+                                             AgentContext.instance.config.core_agent_full_name())
         self.downloader = CoreAgentDownloader(self.core_agent_dir,
-                                              AgentContext.instance().config.core_agent_full_name())
+                                              AgentContext.instance.config.core_agent_full_name())
 
     def launch(self):
-        if AgentContext.instance().config.value('core_agent_launch') is not True:
+        if AgentContext.instance.config.value('core_agent_launch') is not True:
             logger.debug("Not attempting to launch Core Agent "
                          "due to 'core_agent_launch' setting.")
             return
 
         if self.verify() is not True:
-            if AgentContext.instance().config.value('core_agent_download') is True:
+            if AgentContext.instance.config.value('core_agent_download') is True:
                 self.download()
             else:
                 logger.debug("Not attempting to download Core Agent due "
@@ -75,22 +75,22 @@ class CoreAgentManager:
         return ['--daemonize', 'true']
 
     def socket_path(self):
-        socket_path = AgentContext.instance().config.value('socket_path')
+        socket_path = AgentContext.instance.config.value('socket_path')
         return ['--socket', socket_path]
 
     def log_level(self):
-        level = AgentContext.instance().config.value('log_level')
+        level = AgentContext.instance.config.value('log_level')
         return ['--log-level', level]
 
     def log_file(self):
-        path = AgentContext.instance().config.value('log_file')
+        path = AgentContext.instance.config.value('log_file')
         if path is not None:
             return ['--log-file', path]
         else:
             return []
 
     def config_file(self):
-        path = AgentContext.instance().config.value('config_file')
+        path = AgentContext.instance.config.value('config_file')
         if path is not None:
             return ['--config-file', path]
         else:
@@ -188,7 +188,7 @@ class CoreAgentDownloader():
                 core_agent_full_name=self.core_agent_full_name)
 
     def root_url(self):
-        return AgentContext.instance().config.value('download_url')
+        return AgentContext.instance.config.value('download_url')
 
 
 class CoreAgentManifest:

src/scout_apm/bottle/__init__.py
@@ -30,7 +30,7 @@ class ScoutPlugin(object):
         scout_apm.core.install()
 
     def apply(self, callback, context):
-        if AgentContext.instance().config.value('monitor') is not True:
+        if AgentContext.instance.config.value('monitor') is not True:
             return callback
 
         def wrapper(*args, **kwargs):

src/scout_apm/core/__init__.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 import logging
 from os import getpid
 
+from scout_apm.core.config.config import ScoutConfig
 from scout_apm.core.context import AgentContext
 from scout_apm.core.core_agent_manager import CoreAgentManager
 from scout_apm.core.metadata import AppMetadata
@@ -12,7 +13,10 @@ logger = logging.getLogger(__name__)
 
 
 def install():
-    if not AgentContext.instance().config.value('monitor'):
+    config = ScoutConfig()
+    context = AgentContext.build(config=config)
+
+    if not context.config.value('monitor'):
         logger.info("APM Not Launching on PID: %s - Configuration 'monitor' is not true", getpid())
         return False
 

src/scout_apm/core/context.py
@@ -2,17 +2,25 @@ from __future__ import absolute_import
 import logging
 
 from .config.config import ScoutConfig
+from .instrument_manager import InstrumentManager
 from .socket import CoreAgentSocket
-from .thread_local import ThreadLocalSingleton
 
 # Logging
 logger = logging.getLogger(__name__)
 
 
-class AgentContext(ThreadLocalSingleton):
+class AgentContext():
+    instance = None
+
     def __init__(self, *args, **kwargs):
         self.config = kwargs.get('config', ScoutConfig())
 
     @classmethod
+    def build(cls, *args, **kwargs):
+        cls.instance = AgentContext(*args, **kwargs)
+        return cls.instance
+
+    @classmethod
     def socket(cls):
         return CoreAgentSocket.instance(scout_config=ScoutConfig())
+

src/scout_apm/core/metadata.py
@@ -34,17 +34,17 @@ class AppMetadata():
                                                            version_tuple[1],
                                                            version_tuple[2]),
                     'server_time':        datetime.utcnow().isoformat() + 'Z',
-                    'framework':          AgentContext.instance().config.value('framework'),
-                    'framework_version':  AgentContext.instance().config.value('framework_version'),
+                    'framework':          AgentContext.instance.config.value('framework'),
+                    'framework_version':  AgentContext.instance.config.value('framework_version'),
                     'environment':        '',
-                    'app_server':         AgentContext.instance().config.value('app_server'),
-                    'hostname':           AgentContext.instance().config.value('hostname'),
+                    'app_server':         AgentContext.instance.config.value('app_server'),
+                    'hostname':           AgentContext.instance.config.value('hostname'),
                     'database_engine':    '',  # Detected
                     'database_adapter':   '',  # Raw
                     'application_name':   '',  # Environment.application_name,
                     'libraries':          cls.get_python_packages_versions(),
                     'paas':               '',
-                    'git_sha':            AgentContext.instance().config.value('revision_sha')}
+                    'git_sha':            AgentContext.instance.config.value('revision_sha')}
         except Exception as e:
             logger.debug('Exception in AppMetadata: %s', repr(e))
 

commit 2205bcbbf739f6e50d57c1cb54755a6d64e5381b

Fix core-agent-manager
Fixes #120. Steps:

* Added a call to `AgentContext.build()` to restore functionality. This is the
  only thing that broke. I used `git bisect` to discover that it was broken in
  6ab579e22ab903bb6b72108bc485550bb151f353, which is when `AgentContext` was
  refactored.
* Refactored to remove the unnecessary extra functions for the subparsers
* Removed unnecessary subparser name validity check, as this is done by
  argparse already
* Restored test coverage checking for the file
* Added some basic tests to get to 100% coverage

setup.cfg
@@ -11,9 +11,7 @@ ignore =
 
 [coverage:run]
 branch = True
-omit =
-    src/scout_apm/core/cli/core_agent_manager.py
-    src/scout_apm/core/monkey.py
+omit = src/scout_apm/core/monkey.py
 
 [flake8]
 # core

src/scout_apm/core/cli/core_agent_manager.py
@@ -4,38 +4,31 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import argparse
 import logging
 
+from scout_apm.core import AgentContext, CoreAgentManager
 
-def download(**kwargs):
-    from scout_apm.core import CoreAgentManager
-
-    core_agent_manager = CoreAgentManager()
-    core_agent_manager.download()
+logger = logging.getLogger(__name__)
 
 
-def launch(**kwargs):
-    from scout_apm.core import CoreAgentManager
-
-    core_agent_manager = CoreAgentManager()
-    core_agent_manager.launch()
-
-
-def main(**kwargs):
+def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-v", "--verbose", help="increase output verbosity", action="count"
     )
 
-    subparsers = parser.add_subparsers(dest="subparser")
+    subparsers = parser.add_subparsers(
+        title="subcommands", description="valid subcommands", dest="subparser"
+    )
     subparsers.add_parser("download")
     subparsers.add_parser("launch")
 
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     if args.verbose is not None:
         if args.verbose >= 2:
-            logging.basicConfig(level=getattr(logging, "DEBUG", None))
-        elif args.verbose == 1:
-            logging.basicConfig(level=getattr(logging, "INFO", None))
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.INFO)
 
-    kwargs = vars(args)
-    globals()[kwargs.pop("subparser")](**kwargs)
+    AgentContext.build()
+    core_agent_manager = CoreAgentManager()
+    getattr(core_agent_manager, args.subparser)()

tests/unit/core/cli/test_core_agent_manager.py
@@ -1,0 +1,39 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+import pytest
+
+from scout_apm.core.cli.core_agent_manager import main
+from tests.compat import mock
+
+
+@pytest.fixture(autouse=True)
+def mock_CoreAgentManager():
+    # Always mock out the actual CoreAgentManager class for these tests, to
+    # keep them quick
+    path = "scout_apm.core.cli.core_agent_manager.CoreAgentManager"
+    with mock.patch(path) as mock_obj:
+        yield mock_obj
+
+
+@mock.patch("scout_apm.core.cli.core_agent_manager.logging.basicConfig")
+@pytest.mark.parametrize(
+    "args, expected_level", [(["-v"], logging.INFO), (["-v", "-v"], logging.DEBUG)]
+)
+def test_logging_verbose(mock_basicConfig, args, expected_level):
+    # Have to use a mock for basicConfig since logging is already configured
+    # during tests
+    main(args + ["download"])
+
+    mock_basicConfig.assert_called_with(level=expected_level)
+
+
+@pytest.mark.parametrize("command", ["download", "launch"])
+def test_command(mock_CoreAgentManager, command):
+    main([command])
+
+    instance = mock_CoreAgentManager.return_value
+    method = getattr(instance, command)
+    method.assert_called_with()