08817946b9e627a650813154f9536d786a4451d7
Adam Johnson <me@adamj.eu>
2019-07-02 11:51:48 +0100

    Add extra Celery tags
    Fixes #34. Tagging with the current *queue* is only *sometimes* possible - often the *exchange* and *routing_key* are provided instead. The task may have come from a particular queue, but its delivery information normally onyl tracks where it was sent at the top of the AMQP funnel by exchange + routing_key, since that's what's needed for retry. Also track the `is_eager` flag which may be useful for determining which way the task was run.
    
    More information on these parameters can be gleaned from [the source](https://github.com/celery/celery/blob/8cf8fae70630954cff3448485588bbe2a77ff3ab/celery/app/task.py#L600).
    
    Updating the tests to use the fixture added in Celery 4.0, but since we support 3.1 this is in a conditional test. Our test suite currently installs the latest version of Celery only, so I tested locally temporarily with Celery 3.1 as well by specifying `celery>=3.1,<3.2` in `tox.ini`. For complete automated testing in the future, we should look at a better requirements management system.

--- src/scout_apm/celery.py
+++ src/scout_apm/celery.py
@@ -7,20 +7,22 @@ import scout_apm.core
 from scout_apm.core.tracked_request import TrackedRequest
 
 
-# TODO: Capture queue.
-# https://stackoverflow.com/questions/22385297/how-to-get-the-queue-in-which-a-task-was-run-celery?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-def prerun_callback(sender=None, headers=None, body=None, **kwargs):
-    name = kwargs["task"].name
+def prerun_callback(task=None, **kwargs):
+    tracked_request = TrackedRequest.instance()
+    tracked_request.mark_real_request()
 
-    tr = TrackedRequest.instance()
-    tr.mark_real_request()
-    span = tr.start_span(operation=("Job/" + name))
-    span.tag("queue", "default")
+    delivery_info = task.request.delivery_info
+    tracked_request.tag("is_eager", delivery_info.get("is_eager", False))
+    tracked_request.tag("exchange", delivery_info.get("exchange", "unknown"))
+    tracked_request.tag("routing_key", delivery_info.get("routing_key", "unknown"))
+    tracked_request.tag("queue", delivery_info.get("queue", "unknown"))
+
+    tracked_request.start_span(operation=("Job/" + task.name))
 
 
-def postrun_callback(sender=None, headers=None, body=None, **kwargs):
-    tr = TrackedRequest.instance()
-    tr.stop_span()
+def postrun_callback(task=None, **kwargs):
+    tracked_request = TrackedRequest.instance()
+    tracked_request.stop_span()
 
 
 def install():
