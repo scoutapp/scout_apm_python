4b3bfd196aaa3eee85e6e22bbf2f4eda94c341c5
Adam Johnson <me@adamj.eu>
2019-07-04 09:56:00 +0100

    Fix core-agent-manager (#202)
    Fixes #120. Steps:
    
    * Added a call to `AgentContext.build()` to restore functionality. This is the
      only thing that broke. I used `git bisect` to discover that it was broken in
      6ab579e22ab903bb6b72108bc485550bb151f353, which is when `AgentContext` was
      refactored.
    * Refactored to remove the unnecessary extra functions for the subparsers
    * Removed unnecessary subparser name validity check, as this is done by
      argparse already
    * Restored test coverage checking for the file
    * Added some basic tests to get to 100% coverage

---
+++ tests/unit/core/cli/test_core_agent_manager.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+import pytest
+
+from scout_apm.core.cli.core_agent_manager import main
+from tests.compat import mock
+
+
+@pytest.fixture(autouse=True)
+def mock_CoreAgentManager():
+    # Always mock out the actual CoreAgentManager class for these tests, to
+    # keep them quick
+    path = "scout_apm.core.cli.core_agent_manager.CoreAgentManager"
+    with mock.patch(path) as mock_obj:
+        yield mock_obj
+
+
+@mock.patch("scout_apm.core.cli.core_agent_manager.logging.basicConfig")
+@pytest.mark.parametrize(
+    "args, expected_level", [(["-v"], logging.INFO), (["-v", "-v"], logging.DEBUG)]
+)
+def test_logging_verbose(mock_basicConfig, args, expected_level):
+    # Have to use a mock for basicConfig since logging is already configured
+    # during tests
+    main(args + ["download"])
+
+    mock_basicConfig.assert_called_with(level=expected_level)
+
+
+@pytest.mark.parametrize("command", ["download", "launch"])
+def test_command(mock_CoreAgentManager, command):
+    main([command])
+
+    instance = mock_CoreAgentManager.return_value
+    method = getattr(instance, command)
+    method.assert_called_with()
