8e441b92a7b101f7ddec66969dee1283721e1795
Adam Johnson <me@adamj.eu>
2019-07-06 12:00:13 +0100

    Tidy Django tests (#208)
    * Don't use DEBUG mode in tests - this means we're testing against a more "production ready" Django app
    * Group asserts properly according to the AAA pattern

--- tests/integration/django_app.py
+++ tests/integration/django_app.py
@@ -13,8 +13,8 @@ config = {
     "DATABASES": {
         "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
     },
-    "DEBUG": True,
     # Enable the following for debugging exceptions:
+    # "DEBUG": True,
     # "DEBUG_PROPAGATE_EXCEPTIONS": True,
     "ROOT_URLCONF": __name__,
     "SECRET_KEY": "********",
