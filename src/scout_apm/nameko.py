10bcb421920260e0e596e3b38e7d42e42eaeb029
Adam Johnson <me@adamj.eu>
2019-07-10 10:16:39 +0100

    Add and test error case, path tracking

--- src/scout_apm/nameko.py
+++ src/scout_apm/nameko.py
@@ -2,15 +2,41 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from nameko.extensions import DependencyProvider
+from nameko.web.handlers import HttpRequestHandler
+from werkzeug.wrappers import Request
 
+import scout_apm.core
 from scout_apm.core.tracked_request import TrackedRequest
 
 
 class ScoutReporter(DependencyProvider):
+    def setup(self):
+        scout_apm.core.install()
+
     def worker_setup(self, worker_ctx):
         tracked_request = TrackedRequest.instance()
         tracked_request.mark_real_request()
-        tracked_request.start_span(operation="Test")
+
+        # Get HTTP details for HTTP handlers
+        if isinstance(worker_ctx.entrypoint, HttpRequestHandler):
+            try:
+                request = worker_ctx.args[0]
+            except IndexError:
+                pass
+            else:
+                if isinstance(request, Request):
+                    tracked_request.tag("path", request.path)
+
+        operation = (
+            "Controller/"
+            + worker_ctx.service.name
+            + "."
+            + worker_ctx.entrypoint.method_name
+        )
+        tracked_request.start_span(operation=operation)
 
     def worker_result(self, worker_ctx, result=None, exc_info=None):
-        TrackedRequest.instance().stop_span()
+        tracked_request = TrackedRequest.instance()
+        if exc_info:
+            tracked_request.tag("error", "true")
+        tracked_request.stop_span()
--- tests/integration/test_nameko.py
+++ tests/integration/test_nameko.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from contextlib import contextmanager
 
+from nameko.containers import get_container_cls
 from nameko.web.handlers import http
 from webtest import TestApp
 
@@ -11,7 +12,7 @@ from scout_apm.nameko import ScoutReporter
 
 
 @contextmanager
-def app_with_scout(container_factory, nameko_config=None, scout_config=None):
+def app_with_scout(nameko_config=None, scout_config=None):
     """
     Context manager that yields a fresh Nameko WSGI app with Scout configured.
     """
@@ -26,37 +27,60 @@ def app_with_scout(container_factory, nameko_config=None, scout_config=None):
         scout = ScoutReporter()
 
         @http("GET", "/")
-        def resource(self, request):
+        def home(self, request):
             return "Welcome home."
+
+        @http("GET", "/crash/")
+        def crash(self, request):
+            raise ValueError("BØØM!")  # non-ASCII
 
     if nameko_config is None:
         nameko_config = {}
-    container = container_factory(Service, nameko_config)
-    container.start()
-    # A bit of introspection to look inside the container and pull out the WSGI
-    # app
-    app = list(container.subextensions)[0].get_wsgi_app()
-
-    # N.B. We're sidestepping the Nameko testing conventions
-    # (https://docs.nameko.io/en/stable/testing.html) to make our tests more
-    # uniform between frameworks
-
+    # Container setup copied from Nameko's container_factory pytest fixture,
+    # which we don't use - see pytest.ini
+    container_cls = get_container_cls(nameko_config)
+    container = container_cls(Service, nameko_config)
     try:
+        container.start()
+
+        # A bit of introspection to look inside the container and pull out the WSGI
+        # app
+        app = list(container.subextensions)[0].get_wsgi_app()
+
+        # N.B. We're sidestepping the Nameko testing conventions
+        # (https://docs.nameko.io/en/stable/testing.html) to make our tests more
+        # uniform between frameworks
+
         yield app
     finally:
+        container.kill()
         Config.reset_all()
 
 
-def test_home(container_factory, tracked_requests):
-    with app_with_scout(container_factory) as app:
+def test_home(tracked_requests):
+    with app_with_scout() as app:
         response = TestApp(app).get("/")
 
     assert response.status_int == 200
     assert response.text == "Welcome home."
     assert len(tracked_requests) == 1
     tracked_request = tracked_requests[0]
-    # assert tracked_request.tags["path"] == "/"
+    assert tracked_request.tags["path"] == "/"
     assert tracked_request.active_spans == []
     assert len(tracked_request.complete_spans) == 1
     span = tracked_request.complete_spans[0]
-    assert span.operation == "Test"
+    assert span.operation == "Controller/myservice.home"
+
+
+def test_server_error(tracked_requests):
+    with app_with_scout() as app:
+        response = TestApp(app).get("/crash/", expect_errors=True)
+
+    assert response.status_int == 500
+    assert len(tracked_requests) == 1
+    tracked_request = tracked_requests[0]
+    assert tracked_request.tags["error"] == "true"
+    assert tracked_request.tags["path"] == "/crash/"
+    assert len(tracked_request.complete_spans) == 1
+    span = tracked_request.complete_spans[0]
+    assert span.operation == "Controller/myservice.crash"
