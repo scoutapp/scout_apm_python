a00c8bc001fb63822f162f47c250b8ec4abe8a45
Adam Johnson <me@adamj.eu>
2019-08-11 09:19:25 +0100

    Improve Django admin views' traced names
    Fixes #55.

--- tests/integration/django_app.py
+++ tests/integration/django_app.py
@@ -1,12 +1,8 @@
 # coding: utf-8
-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import django
 from django.conf import settings
-from django.db import connection
-from django.http import HttpResponse
-from django.template import engines
 
 config = {
     "ALLOWED_HOSTS": ["*"],
@@ -18,20 +14,56 @@ config = {
     # "DEBUG_PROPAGATE_EXCEPTIONS": True,
     "ROOT_URLCONF": __name__,
     "SECRET_KEY": "********",
-    "TEMPLATES": [{"BACKEND": "django.template.backends.django.DjangoTemplates"}],
+    "TEMPLATES": [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "APP_DIRS": True,
+            "OPTIONS": {
+                "context_processors": [
+                    "django.contrib.auth.context_processors.auth",
+                    "django.contrib.messages.context_processors.messages",
+                ]
+            },
+        }
+    ],
     "TIME_ZONE": "America/Chicago",
     # Setup as per https://docs.scoutapm.com/#django but *without* the settings
     # - these are temporarily set by app_with_scout() to avoid state leak
-    "INSTALLED_APPS": ["scout_apm.django"],
+    "INSTALLED_APPS": [
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.messages",
+        "django.contrib.sessions",
+        "scout_apm.django",
+    ],
 }
 
 if django.VERSION > (1, 10):
-    config["MIDDLEWARE"] = []
+    config["MIDDLEWARE"] = [
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+    ]
 else:
-    config["MIDDLEWARE_CLASSES"] = []
+    config["MIDDLEWARE_CLASSES"] = [
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+    ]
 
 
 settings.configure(**config)
+
+if True:
+    # Old versions of Django, at least 1.8, need settings configured before
+    # other bits are imported such as Admin. Hence do the imports here, under
+    # an 'if True' to appease isort.
+    from django.contrib import admin
+    from django.db import connection
+    from django.http import HttpResponse
+    from django.template import engines
+    from django.utils.functional import SimpleLazyObject
 
 
 def home(request):
@@ -65,23 +97,31 @@ def template(request):
     return HttpResponse(template.render(context))
 
 
-try:
-    from django.urls import path
+@SimpleLazyObject
+def urlpatterns():
+    """
+    URL's as a lazy object because they touch admin.site.urls and that isn't
+    ready until django.setup() has been called
+    """
+    try:
+        from django.urls import path
 
-    urlpatterns = [
-        path("", home),
-        path("hello/", hello),
-        path("crash/", crash),
-        path("sql/", sql),
-        path("template/", template),
-    ]
-except ImportError:  # Django < 2.0
-    from django.conf.urls import url
+        return [
+            path("", home),
+            path("hello/", hello),
+            path("crash/", crash),
+            path("sql/", sql),
+            path("template/", template),
+            path("admin/", admin.site.urls),
+        ]
+    except ImportError:  # Django < 2.0
+        from django.conf.urls import url
 
-    urlpatterns = [
-        url(r"^$", home),
-        url(r"^hello/$", hello),
-        url(r"^crash/$", crash),
-        url(r"^sql/$", sql),
-        url(r"^template/$", template),
-    ]
+        return [
+            url(r"^$", home),
+            url(r"^hello/$", hello),
+            url(r"^crash/$", crash),
+            url(r"^sql/$", sql),
+            url(r"^template/$", template),
+            url(r"^admin/", admin.site.urls),
+        ]
