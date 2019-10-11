089ae29050e9dd2cf3a5c14b668c675808b0697a
Adam Johnson <me@adamj.eu>
2019-10-11 17:13:21 +0100

    src

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup_args = {
     "zip_safe": False,
     "python_requires": ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
     "packages": find_packages("src"),
-    "package_dir": {"": "src"},
+    "package_dir": {str(""): str("src")},
     "py_modules": [os.splitext(os.basename(path))[0] for path in glob("src/*.py")],
     "ext_modules": [
         Extension(
