e30b4b8a400226a393b239fea8cea119b8e5d771
Adam Johnson <me@adamj.eu>
2019-10-23 10:04:19 +0100

    working asgi application example with wrapper

--- src/scout_apm/asgi/__init__.py
+++ src/scout_apm/asgi/__init__.py
@@ -1,2 +1,30 @@
-def asgi_importable():
-    return True
+from urllib.parse import parse_qsl
+
+from scout_apm.core.tracked_request import TrackedRequest
+from scout_apm.core.web_requests import create_filtered_path
+
+
+def wrap_asgi_application(application):
+    async def scout_wrapped_application(scope, receive, send):
+        is_http = scope["type"] == "http"
+
+        if is_http:
+            tracked_request = TrackedRequest.instance()
+            tracked_request.mark_real_request()
+            operation = "Controller/" + scope["method"] + "_" + scope["path"]
+            tracked_request.start_span(operation=operation)
+
+            tracked_request.tag(
+                "path",
+                create_filtered_path(scope["path"], parse_qsl(scope["query_string"])),
+            )
+
+        try:
+            await application(scope, receive, send)
+        except Exception as exc:
+            tracked_request.tag("error", "true")
+            raise exc
+        finally:
+            tracked_request.stop_span()
+
+    return scout_wrapped_application
--- tests/integration/test_asgi_py36plus.py
+++ tests/integration/test_asgi_py36plus.py
@@ -1,8 +1,104 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from scout_apm.asgi import asgi_importable
+import functools
+from contextlib import contextmanager
+
+import asgiref.sync
+from asgiref.testing import ApplicationCommunicator
+
+import scout_apm.core
+from scout_apm.api import Config
+from scout_apm.asgi import wrap_asgi_application
 
 
-async def test_asgi_importable():
-    assert asgi_importable()
+@contextmanager
+def app_with_scout(scout_config=None):
+    """
+    Context manager that configures and installs the Scout plugin for a basic
+    ASGI (3) application.
+    """
+    if scout_config is None:
+        scout_config = {}
+
+    # config["SCOUT_CORE_AGENT_LAUNCH"] = False
+    # config.setdefault("SCOUT_MONITOR", True)
+    # Disable Flask's error page to improve debugging
+    # config.setdefault("PROPAGATE_EXCEPTIONS", True)
+    Config.set(**scout_config)
+
+    scout_apm.core.install()
+
+    async def app(scope, receive, send):
+        assert scope["type"] == "http"
+        body = await receive()
+        assert body["type"] == "http.request"
+        print(scope)
+        if scope["path"] == "/":
+            body = b"Welcome home."
+        elif scope["path"] == "/hello/":
+            body = b"Hello World!"
+        elif scope["path"] == "/crash/":
+            raise ValueError("BØØM!")  # non-ASCII
+
+        await send({"type": "http.response.start", "status": 200, "headers": []})
+        await send({"type": "http.response.body", "body": body, "more_body": False})
+
+    wrapped_app = wrap_asgi_application(app)
+
+    try:
+        yield wrapped_app
+    finally:
+        Config.reset_all()
+
+
+def async_to_sync(func):
+    """
+    Wrap async_to_sync with another function because Pytest complains about
+    collecting the resulting callable object as a test because it's not a true
+    function:
+
+    PytestCollectionWarning: cannot collect 'test_foo' because it is not a
+    function.
+    """
+    sync_func = asgiref.sync.async_to_sync(func)
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return sync_func(*args, **kwargs)
+
+    return wrapper
+
+
+def get_scope(**kwargs):
+    return {
+        "type": "http",
+        "asgi": {"version": "3.0", "spec_version": "2.1"},
+        "http_version": "1.1",
+        "method": "GET",
+        "query_string": b"",
+        "server": ("testserver", 80),
+        **kwargs,
+    }
+
+
+@async_to_sync
+async def test_home(tracked_requests):
+    with app_with_scout() as app:
+        communicator = ApplicationCommunicator(app, get_scope(path="/"))
+        await communicator.send_input({"type": "http.request"})
+        # Read the response.
+        response_start = await communicator.receive_output()
+        assert response_start["type"] == "http.response.start"
+        assert response_start["status"] == 200
+        assert response_start["headers"] == []
+        response_body = await communicator.receive_output()
+        assert response_body["type"] == "http.response.body"
+        assert response_body["body"] == b"Welcome home."
+
+    assert len(tracked_requests) == 1
+    tracked_request = tracked_requests[0]
+    assert len(tracked_request.complete_spans) == 1
+    assert tracked_request.tags["path"] == "/"
+    span = tracked_request.complete_spans[0]
+    assert span.operation == "Controller/GET_/"
