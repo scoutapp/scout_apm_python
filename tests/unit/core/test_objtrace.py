a490ce94af6fe750651c68854718e7a664be2544
Adam Johnson <me@adamj.eu>
2019-10-28 23:52:34 +0000

    Improve objtrace tests (#315)
    Ensure key pathways in the C level extension get tested.

--- tests/unit/core/test_objtrace.py
+++ tests/unit/core/test_objtrace.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import sys
+
 import pytest
 
 from scout_apm.core import objtrace
@@ -22,21 +24,59 @@ def test_enables_and_disabled():
     objtrace.disable()
 
 
+def test_enable_twice():
+    objtrace.enable()
+    objtrace.enable()
+
+
+def test_disable_twice():
+    objtrace.disable()
+    objtrace.disable()
+
+
+def test_get_counts():
+    objtrace.enable()
+    counts = objtrace.get_counts()
+    assert isinstance(counts, tuple)
+    assert len(counts) == 4
+    assert all(isinstance(x, int) for x in counts)
+
+
 @skip_if_objtrace_not_extension
-def test_allocation_counts():
+def test_get_counts_allocations():
     lists = []
     objtrace.enable()
-    for _ in range(100):
-        lists.append([1])
+    lists.extend([1] for _ in range(100))
     objtrace.disable()
-    c = objtrace.get_counts()
-    assert c[0] > 0
+    counts = objtrace.get_counts()
+    assert counts[0] > 0
 
 
 @skip_if_objtrace_not_extension
-def test_frees_counts():
+@pytest.mark.skipif(
+    sys.version_info < (3, 5), reason="Mulitple allocations on Python 3.5+ only"
+)
+def test_get_counts_multiple_allocations():
+    objtrace.enable()
+    bytes("123")
+    counts = objtrace.get_counts()
+    assert counts[1] > 0
+
+
+@skip_if_objtrace_not_extension
+def test_get_counts_reallocations():
+    items = []
+    objtrace.enable()
+    for i in range(1000):
+        items.append(i)
+    counts = objtrace.get_counts()
+    assert counts[2] > 0
+
+
+@skip_if_objtrace_not_extension
+def test_get_counts_frees():
     objtrace.enable()
     for x in (1, 2, 3):
         y = x  # noqa: F841
-    c = objtrace.get_counts()
-    assert c[3] > 0
+    counts = objtrace.get_counts()
+    assert counts[3] > 0
