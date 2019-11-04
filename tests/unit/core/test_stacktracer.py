ac52408c9964dbe5671587169505270f92a1ebab
Adam Johnson <me@adamj.eu>
2019-11-04 10:19:24 +0000

    Remove dead Django custom template tag instrumentation
    This code hasn't been active since it was added in the first commit: 4de21f8ea228a082d4f039c0c991ee41dfb6f9d8. Remove it for now, though it could provide a useful starting point for reimplementation in issue #313.

--- tests/unit/core/test_stacktracer.py
+++ tests/unit/core/test_stacktracer.py
@@ -1,11 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import pytest
-
-from scout_apm.core.stacktracer import trace_function, trace_method
-from scout_apm.core.tracked_request import TrackedRequest
-from tests.compat import mock
+from scout_apm.core.stacktracer import trace_method
 
 
 class TraceMe(object):
@@ -15,46 +11,6 @@ class TraceMe(object):
 
 def trace_me(*args, **kwargs):
     return args, kwargs
-
-
-@pytest.fixture
-def tracked_request():
-    request = TrackedRequest.instance()
-    request.start_span()  # prevent request from finalizing
-    try:
-        yield request
-    finally:
-        request.stop_span()
-
-
-def test_trace_function(tracked_request):
-    traced = trace_function(trace_me, ("Test/Function", {"name": "trace_me"}))
-    traced()
-
-    span = tracked_request.complete_spans[0]
-    assert span.operation == "Test/Function/trace_me"
-
-
-def test_trace_function_callable_info(tracked_request):
-    traced = trace_function(trace_me, lambda: ("Test/Function", {"name": "trace_me"}))
-    traced()
-
-    span = tracked_request.complete_spans[0]
-    assert span.operation == "Test/Function/trace_me"
-
-
-@mock.patch("scout_apm.core.stacktracer.wrapt.decorator", side_effect=RuntimeError)
-def test_trace_function_exception(mock_decorator, tracked_request):
-    traced = trace_function(trace_me, lambda: ("Test/Function", {"name": "trace_me"}))
-    assert traced is trace_me  # patching failed
-
-
-def test_trace_function_no_name(tracked_request):
-    traced = trace_function(trace_me, ("Test/Function", {"name": None}))
-    traced()
-
-    span = tracked_request.complete_spans[0]
-    assert span.operation == "Test/Function"
 
 
 def test_trace_method(tracked_request):
