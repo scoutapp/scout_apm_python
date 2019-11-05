1649252a674aa91a64688b652f481bedd7c82781
Adam Johnson <me@adamj.eu>
2019-11-05 21:37:34 +0000

    Convert Bottle decorator to wrapt (#360)
    Should have been done as part of #311. Defining the decorator as an outer function also provides a small performance improvement.

--- src/scout_apm/bottle.py
+++ src/scout_apm/bottle.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import wrapt
 from bottle import request
 
 import scout_apm.core
@@ -34,57 +35,55 @@ class ScoutPlugin(object):
     def apply(self, callback, context):
         if not scout_config.value("monitor"):
             return callback
+        return wrap_callback(callback)
 
-        def wrapper(*args, **kwargs):
-            tracked_request = TrackedRequest.instance()
-            tracked_request.is_real_request = True
 
-            path = request.path
-            # allitems() is an undocumented bottle internal
-            tracked_request.tag(
-                "path", create_filtered_path(path, request.query.allitems())
-            )
-            if ignore_path(path):
-                tracked_request.tag("ignore_transaction", True)
+@wrapt.decorator
+def wrap_callback(wrapped, instance, args, kwargs):
+    tracked_request = TrackedRequest.instance()
+    tracked_request.is_real_request = True
 
-            if request.route.name is not None:
-                controller_name = request.route.name
-            else:
-                controller_name = request.route.rule
-            if controller_name == "/":
-                controller_name = "/home"
-            if not controller_name.startswith("/"):
-                controller_name = "/" + controller_name
-            tracked_request.start_span(
-                operation="Controller{}".format(controller_name),
-                should_capture_backtrace=False,
-            )
+    path = request.path
+    # allitems() is an undocumented bottle internal
+    tracked_request.tag("path", create_filtered_path(path, request.query.allitems()))
+    if ignore_path(path):
+        tracked_request.tag("ignore_transaction", True)
 
-            # Determine a remote IP to associate with the request. The
-            # value is spoofable by the requester so this is not suitable
-            # to use in any security sensitive context.
-            user_ip = (
-                request.headers.get("x-forwarded-for", "").split(",")[0]
-                or request.headers.get("client-ip", "").split(",")[0]
-                or request.environ.get("REMOTE_ADDR")
-            )
-            tracked_request.tag("user_ip", user_ip)
+    if request.route.name is not None:
+        controller_name = request.route.name
+    else:
+        controller_name = request.route.rule
+    if controller_name == "/":
+        controller_name = "/home"
+    if not controller_name.startswith("/"):
+        controller_name = "/" + controller_name
+    tracked_request.start_span(
+        operation="Controller{}".format(controller_name), should_capture_backtrace=False
+    )
 
-            tracked_queue_time = False
-            queue_time = request.headers.get(
-                "x-queue-start", ""
-            ) or request.headers.get("x-request-start", "")
-            tracked_queue_time = track_request_queue_time(queue_time, tracked_request)
-            if not tracked_queue_time:
-                amazon_queue_time = request.headers.get("x-amzn-trace-id", "")
-                track_amazon_request_queue_time(amazon_queue_time, tracked_request)
+    # Determine a remote IP to associate with the request. The
+    # value is spoofable by the requester so this is not suitable
+    # to use in any security sensitive context.
+    user_ip = (
+        request.headers.get("x-forwarded-for", "").split(",")[0]
+        or request.headers.get("client-ip", "").split(",")[0]
+        or request.environ.get("REMOTE_ADDR")
+    )
+    tracked_request.tag("user_ip", user_ip)
 
-            try:
-                return callback(*args, **kwargs)
-            except Exception:
-                tracked_request.tag("error", "true")
-                raise
-            finally:
-                tracked_request.stop_span()
+    tracked_queue_time = False
+    queue_time = request.headers.get("x-queue-start", "") or request.headers.get(
+        "x-request-start", ""
+    )
+    tracked_queue_time = track_request_queue_time(queue_time, tracked_request)
+    if not tracked_queue_time:
+        amazon_queue_time = request.headers.get("x-amzn-trace-id", "")
+        track_amazon_request_queue_time(amazon_queue_time, tracked_request)
 
-        return wrapper
+    try:
+        return wrapped(*args, **kwargs)
+    except Exception:
+        tracked_request.tag("error", "true")
+        raise
+    finally:
+        tracked_request.stop_span()
