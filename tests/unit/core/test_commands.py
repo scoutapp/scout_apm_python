scoutapp/scout_apm_python

commit b9a7378d2dc3fcd96619c456b6d799d6a0f80b77

Provide a Python wrapper module for objtrace (#303)
Rather than testing all over the place whether it's importable, perform the logic once in a Python wrapper that provides dummy implementations when the C extension is unavailable. Also deduplicate the test wrappers that check if it's importable.

src/scout_apm/core/objtrace.py
@@ -1,0 +1,25 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# Try use the C extension, but if it isn't available, provide a dummy
+# implementation.
+
+try:
+    from scout_apm.core._objtrace import enable, disable, get_counts
+except ImportError:  # pragma: no cover
+
+    def enable():
+        pass
+
+    def disable():
+        pass
+
+    def get_counts():
+        return (0, 0, 0, 0)
+
+    def reset_counts():
+        pass
+
+    is_extension = False
+else:  # pragma: no cover
+    is_extension = True

tests/tools.py
@@ -1,0 +1,13 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest
+
+from scout_apm.core import objtrace
+
+skip_if_objtrace_not_extension = pytest.mark.skipif(
+    not objtrace.is_extension, reason="Requires objtrace C extension"
+)
+skip_if_objtrace_is_extension = pytest.mark.skipif(
+    not objtrace.is_extension, reason="Requires no objtrace C extension"
+)

tests/unit/core/test_commands.py
@@ -7,11 +7,7 @@ import pytest
 
 from scout_apm.core import commands
 from scout_apm.core.tracked_request import TrackedRequest
-
-try:
-    from scout_apm.core import objtrace
-except ImportError:
-    objtrace = None
+from tests.tools import skip_if_objtrace_not_extension
 
 REQUEST_ID = "req-97c1d72c-5519-4665-85d3-1ac21af39b63"
 SPAN_ID = "span-7dbb0712-e3c5-4b73-b317-f8d2114c5993"
@@ -258,9 +254,7 @@ def test_batch_command_from_tracked_request_with_tag():
     }
 
 
-@pytest.mark.skipif(
-    objtrace is None, reason="Allocation tags only there when objtrace works"
-)
+@skip_if_objtrace_not_extension
 def test_batch_command_from_tracked_request_with_span():
     tracked_request = TrackedRequest()
     tracked_request.start_span()
@@ -324,9 +318,7 @@ def test_batch_command_from_tracked_request_with_span():
     }
 
 
-@pytest.mark.skipif(
-    objtrace is not None, reason="Allocation tags only there when objtrace works"
-)
+@skip_if_objtrace_not_extension
 def test_batch_command_from_tracked_request_with_span_no_objtrace():
     tracked_request = TrackedRequest()
     tracked_request.start_span()

tests/unit/core/test_objtrace.py
@@ -3,19 +3,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import pytest
 
-try:
-    from scout_apm.core import objtrace
-except ImportError:
-    objtrace = None
-
-if objtrace is None:
-    pytest.skip("objtrace extension isn't available", allow_module_level=True)
+from scout_apm.core import objtrace
+from tests.tools import skip_if_objtrace_not_extension
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def reset_objtrace_counts():
-    if objtrace is None:
-        return
     objtrace.reset_counts()
     try:
         yield
@@ -23,13 +16,14 @@ def reset_objtrace_counts():
         objtrace.reset_counts()
 
 
-def test_enables_and_disabled(reset_objtrace_counts):
+def test_enables_and_disabled():
     objtrace.enable()
     objtrace.get_counts()
     objtrace.disable()
 
 
-def test_allocation_counts(reset_objtrace_counts):
+@skip_if_objtrace_not_extension
+def test_allocation_counts():
     lists = []
     objtrace.enable()
     for _ in range(100):
@@ -39,7 +33,8 @@ def test_allocation_counts(reset_objtrace_counts):
     assert c[0] > 0
 
 
-def test_frees_counts(reset_objtrace_counts):
+@skip_if_objtrace_not_extension
+def test_frees_counts():
     objtrace.enable()
     for x in (1, 2, 3):
         y = x  # noqa: F841

setup.py
@@ -19,7 +19,7 @@ compile_extensions = (
 if compile_extensions:
     ext_modules = [
         Extension(
-            str("scout_apm.core.objtrace"), [str("src/scout_apm/core/ext/objtrace.c")]
+            str("scout_apm.core._objtrace"), [str("src/scout_apm/core/_objtrace.c")]
         )
     ]
 else:

src/scout_apm/core/__init__.py
@@ -5,16 +5,12 @@ import logging
 import sys
 from os import getpid
 
+from scout_apm.core import objtrace
 from scout_apm.core.config import ScoutConfig
 from scout_apm.core.context import AgentContext
 from scout_apm.core.core_agent_manager import CoreAgentManager
 from scout_apm.core.instrument_manager import InstrumentManager
 from scout_apm.core.metadata import AppMetadata
-
-try:
-    from scout_apm.core import objtrace
-except ImportError:
-    objtrace = None
 
 logger = logging.getLogger(__name__)
 
@@ -37,8 +33,7 @@ def install(*args, **kwargs):
 
     InstrumentManager().install_all()
 
-    if objtrace is not None:
-        objtrace.enable()
+    objtrace.enable()
 
     logger.debug("APM Launching on PID: %s", getpid())
     launched = CoreAgentManager().launch()

src/scout_apm/core/tracked_request.py
@@ -5,17 +5,12 @@ import datetime as dt
 import logging
 from uuid import uuid4
 
-import scout_apm.core.backtrace
+from scout_apm.core import backtrace, objtrace
 from scout_apm.core.commands import BatchCommand
 from scout_apm.core.context import AgentContext
 from scout_apm.core.n_plus_one_call_set import NPlusOneCallSet
 from scout_apm.core.samplers import Memory, Samplers
 from scout_apm.core.thread_local import ThreadLocalSingleton
-
-try:
-    from scout_apm.core import objtrace
-except ImportError:
-    objtrace = None
 
 logger = logging.getLogger(__name__)
 
@@ -166,10 +161,7 @@ class Span(object):
         self.ignore_children = ignore_children
         self.parent = parent
         self.tags = {}
-        if objtrace is not None:
-            self.start_objtrace_counts = objtrace.get_counts()
-        else:
-            self.start_objtrace_counts = (0, 0, 0, 0)
+        self.start_objtrace_counts = objtrace.get_counts()
         self.end_objtrace_counts = (0, 0, 0, 0)
 
     def __repr__(self):
@@ -180,10 +172,7 @@ class Span(object):
 
     def stop(self):
         self.end_time = dt.datetime.utcnow()
-        if objtrace is not None:
-            self.end_objtrace_counts = objtrace.get_counts()
-        else:
-            self.end_objtrace_counts = (0, 0, 0, 0)
+        self.end_objtrace_counts = objtrace.get_counts()
 
     def tag(self, key, value):
         if key in self.tags:
@@ -215,7 +204,7 @@ class Span(object):
             self.capture_backtrace()
 
     def add_allocation_tags(self):
-        if objtrace is None:
+        if not objtrace.is_extension:
             return
 
         start_allocs = (
@@ -247,5 +236,4 @@ class Span(object):
         self.tag("stop_allocations", end_allocs)
 
     def capture_backtrace(self):
-        stack = scout_apm.core.backtrace.capture()
-        self.tag("stack", stack)
+        self.tag("stack", backtrace.capture())

commit f6054201f8c75027277fc9d350b36ef6aa1fe1c3

Fix objtrace C extension
Broken in #303 and not detected due to test suite skipping the tests, which need skipping on Python 2.7 because the extension isn't compatible with it.

src/scout_apm/core/_objtrace.c
@@ -128,7 +128,7 @@ objtrace_reset_counts(PyObject *module)
 }
 
 PyDoc_STRVAR(module_doc,
-"scout_apm.core.objtrace module.");
+"scout_apm.core._objtrace module.");
 
 static PyMethodDef module_methods[] = {
     {"enable",
@@ -148,7 +148,7 @@ static PyMethodDef module_methods[] = {
 
 static struct PyModuleDef module_def = {
     PyModuleDef_HEAD_INIT,
-    "scout_apm.core.objtrace",
+    "scout_apm.core._objtrace",
     module_doc,
     0, /* non-negative size to be able to unload the module */
     module_methods,
@@ -159,7 +159,7 @@ static struct PyModuleDef module_def = {
 };
 
 PyMODINIT_FUNC
-PyInit_objtrace(void)
+PyInit__objtrace(void)
 {
     PyObject *m, *version;
 

src/scout_apm/core/objtrace.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # implementation.
 
 try:
-    from scout_apm.core._objtrace import enable, disable, get_counts
+    from scout_apm.core._objtrace import enable, disable, get_counts, reset_counts
 except ImportError:  # pragma: no cover
 
     def enable():

tests/integration/test_api.py
@@ -73,7 +73,7 @@ def test_instrument_decorator_default_tags(tracked_request):
 
     assert len(tracked_request.active_spans) == 0
     assert len(tracked_request.complete_spans) == 1
-    assert tracked_request.complete_spans[0].tags == {"x": 99}
+    assert tracked_request.complete_spans[0].tags["x"] == 99
 
 
 def test_instrument_non_ascii_params(tracked_request):

tests/tools.py
@@ -18,7 +18,7 @@ skip_if_objtrace_not_extension = pytest.mark.skipif(
     not objtrace.is_extension, reason="Requires objtrace C extension"
 )
 skip_if_objtrace_is_extension = pytest.mark.skipif(
-    not objtrace.is_extension, reason="Requires no objtrace C extension"
+    objtrace.is_extension, reason="Requires no objtrace C extension"
 )
 
 

tests/unit/core/test_commands.py
@@ -328,7 +328,7 @@ def test_batch_command_from_tracked_request_with_span_no_objtrace():
     command = commands.BatchCommand.from_tracked_request(tracked_request)
 
     message_commands = command.message()["BatchCommand"]["commands"]
-    assert len(message_commands) == 4
+    assert len(message_commands) == 7
     assert message_commands[0] == {
         "StartRequest": {"request_id": REQUEST_ID, "timestamp": START_TIME_STR}
     }
@@ -342,12 +342,39 @@ def test_batch_command_from_tracked_request_with_span_no_objtrace():
         }
     }
     assert message_commands[2] == {
+        "TagSpan": {
+            "timestamp": START_TIME_STR,
+            "request_id": REQUEST_ID,
+            "span_id": SPAN_ID,
+            "tag": "allocations",
+            "value": 0,
+        }
+    }
+    assert message_commands[3] == {
+        "TagSpan": {
+            "timestamp": START_TIME_STR,
+            "request_id": REQUEST_ID,
+            "span_id": SPAN_ID,
+            "tag": "start_allocations",
+            "value": 0,
+        }
+    }
+    assert message_commands[4] == {
+        "TagSpan": {
+            "timestamp": START_TIME_STR,
+            "request_id": REQUEST_ID,
+            "span_id": SPAN_ID,
+            "tag": "stop_allocations",
+            "value": 0,
+        }
+    }
+    assert message_commands[5] == {
         "StopSpan": {
             "request_id": REQUEST_ID,
             "span_id": SPAN_ID,
             "timestamp": END_TIME_STR,
         }
     }
-    assert message_commands[3] == {
+    assert message_commands[6] == {
         "FinishRequest": {"request_id": REQUEST_ID, "timestamp": END_TIME_STR}
     }

tests/unit/core/test_objtrace.py
@@ -54,11 +54,11 @@ def test_get_counts_allocations():
 
 @skip_if_objtrace_not_extension
 @pytest.mark.skipif(
-    sys.version_info < (3, 5), reason="Mulitple allocations on Python 3.5+ only"
+    sys.version_info < (3, 5), reason="Multiple allocations on Python 3.5+ only"
 )
 def test_get_counts_multiple_allocations():
     objtrace.enable()
-    bytes("123")
+    bytes(123)
     counts = objtrace.get_counts()
     assert counts[1] > 0
 