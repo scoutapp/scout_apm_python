3e2b3a11ba20b53500a0516d412c524b1b8f9456
Adam Johnson <me@adamj.eu>
2019-11-25 10:05:07 +0000

    Add full assertions to Flask SQLAlchemy tests

--- tests/integration/test_flask_sqlalchemy.py
+++ tests/integration/test_flask_sqlalchemy.py
@@ -4,27 +4,45 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from contextlib import contextmanager
 
 from flask_sqlalchemy import SQLAlchemy
+from webtest import TestApp
 
 from scout_apm.flask.sqlalchemy import instrument_sqlalchemy
-from tests.integration.test_flask import app_with_scout
+from tests.integration.test_flask import app_with_scout as flask_app_with_scout
 
 
 @contextmanager
-def conn_with_scout():
-    with app_with_scout() as app:
+def app_with_scout():
+    with flask_app_with_scout() as app:
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
         db = SQLAlchemy(app)
         # Setup according to https://docs.scoutapm.com/#flask-sqlalchemy
         instrument_sqlalchemy(db)
         conn = db.engine.connect()
+
+        @app.route("/sqlalchemy/")
+        def sqlalchemy():
+            result = conn.execute("SELECT 'Hello from the DB!'")
+            return list(result)[0][0]
+
         try:
-            yield conn
+            yield app
         finally:
             conn.close()
 
 
-def test_hello():
-    with conn_with_scout() as conn:
-        result = conn.execute("SELECT 'Hello World!'")
-        assert list(result) == [("Hello World!",)]
+def test_sqlalchemy(tracked_requests):
+    with app_with_scout() as app:
+        response = TestApp(app).get("/sqlalchemy/")
+
+    assert response.status_int == 200
+    assert response.text == "Hello from the DB!"
+    assert len(tracked_requests) == 1
+    tracked_request = tracked_requests[0]
+    spans = tracked_request.complete_spans
+    assert len(spans) == 2
+    assert [s.operation for s in spans] == [
+        "SQL/Query",
+        "Controller/tests.integration.test_flask_sqlalchemy.sqlalchemy",
+    ]
+    assert spans[0].tags["db.statement"] == "SELECT 'Hello from the DB!'"
