b3f8173b745ddbdd25d3ac1837eab7c2d200de26
Adam Johnson <me@adamj.eu>
2019-11-29 13:48:15 +0000

    codestyle

--- src/scout_apm/core/backtrace.py
+++ src/scout_apm/core/backtrace.py
@@ -2,16 +2,19 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import sys
-
 import traceback
 
 if sys.version_info >= (3, 5):
+
     def capture():
         return [
             {"file": frame.filename, "line": frame.lineno, "function": frame.name}
             for frame in reversed(traceback.extract_stack()[:-1])
         ]
+
+
 else:
+
     def capture():
         return [
             {"file": frame[0], "line": frame[1], "function": frame[3]}
