63344d0e4547a0610d69d9046863a6aa7c8f2e55
Adam Johnson <me@adamj.eu>
2020-05-22 07:01:30 +0100

    Fix tests on Python 2

--- tests/unit/core/test_metadata.py
+++ tests/unit/core/test_metadata.py
@@ -2,12 +2,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import sys
-from types import SimpleNamespace
 
 import pytest
 
 from scout_apm.core.metadata import get_python_packages_versions, report_app_metadata
-from tests.compat import mock
+from tests.compat import SimpleNamespace, mock
 from tests.tools import pretend_package_unavailable
 
 
