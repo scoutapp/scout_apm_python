7fc43be42bb9484998c93c4e69965e701b47c886
Tim Schilling <schillingt@better-simple.com>
2022-02-08 09:35:42 -0600

    Version 2.24.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ else:
 
 setup(
     name="scout_apm",
-    version="2.23.5",
+    version="2.24.0",
     description="Scout Application Performance Monitoring Agent",
     long_description=long_description,
     long_description_content_type="text/markdown",
