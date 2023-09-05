97a1a2d5e49966d633c48f0af716d2a7f298c4f8
Mitch Hartweg <mitchell@scoutapm.com>
2023-09-05 13:39:43 -0600

    bump version to 3.0.0

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ else:
 
 setup(
     name="scout_apm",
-    version="2.26.1",
+    version="3.0.0",
     description="Scout Application Performance Monitoring Agent",
     long_description=long_description,
     long_description_content_type="text/markdown",
