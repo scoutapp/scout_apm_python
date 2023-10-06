b4401f00bfed8daa08c0c678d2fa013b0134ff3e
Quinn Milionis <35316732+quinnmil@users.noreply.github.com>
2023-10-06 14:17:08 -0700

    bump version to 3.0.1 and remove broken image link (#778)

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ else:
 
 setup(
     name="scout_apm",
-    version="3.0.0",
+    version="3.0.1",
     description="Scout Application Performance Monitoring Agent",
     long_description=long_description,
     long_description_content_type="text/markdown",
