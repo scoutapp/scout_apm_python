35baa67ce8b0a490733270500a208ce65a13d867
Jack Rothrock <jack@scoutapm.com>
2023-12-18 14:02:34 -0700

    Version 3.1.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ else:
 
 setup(
     name="scout_apm",
-    version="3.0.2",
+    version="3.1.0",
     description="Scout Application Performance Monitoring Agent",
     long_description=long_description,
     long_description_content_type="text/markdown",
