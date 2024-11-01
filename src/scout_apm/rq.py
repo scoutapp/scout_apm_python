c6441370ddc660074a05f2e5d51180e5b5585c08
Lance T. Erickson <lancetarn@gmail.com>
2024-11-01 16:37:04 -0500

    Possible fix

--- src/scout_apm/rq.py
+++ src/scout_apm/rq.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 import datetime as dt
+import logging
 
 import wrapt
 from rq import SimpleWorker as RqSimpleWorker
@@ -13,6 +14,8 @@ from scout_apm.core.tracked_request import TrackedRequest
 
 install_attempted = False
 installed = None
+
+logger = logging.getLogger(__name__)
 
 
 def ensure_scout_installed():
@@ -65,9 +68,12 @@ def wrap_perform(wrapped, instance, args, kwargs):
     tracked_request.is_real_request = True
     tracked_request.tag("task_id", instance.get_id())
     tracked_request.tag("queue", instance.origin)
-    queue_time = (
-        dt.datetime.now(dt.timezone.utc) - instance.enqueued_at
-    ).total_seconds()
+    # rq strips tzinfo from enqueued_at during serde in at least some cases
+    # internally everything uses UTC naive datetimes, so we operate on that
+    # assumption here.
+    if instance.enqueued_at.tzinfo is None:
+        queued_at = instance.enqueued_at.replace(tzinfo=dt.timezone.utc)
+    queue_time = (dt.datetime.now(dt.timezone.utc) - queued_at).total_seconds()
     tracked_request.tag("queue_time", queue_time)
     operation = "Job/{}".format(instance.func_name)
     tracked_request.operation = operation
