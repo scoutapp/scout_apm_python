3d536aef1ad97c033a9159d6bedd46aa5c65d8e2
Jack Rothrock <jrothrock14@gmail.com>
2026-09-17 14:29:01 -0600

    Release 4.0.0
    Cut the pending changelog entries (core-agent 1.6.0 bump + core_agent_ca_cert)
    as version 3.6.0 and bump setup.py 3.5.4 -> 3.6.0. The 3.5.4 changes were never
    released on their own; they ship as part of 3.6.0.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ else:
 
 setup(
     name="scout_apm",
-    version="3.5.4",
+    version="4.0.0",
     description="Scout Application Performance Monitoring Agent",
     long_description=long_description,
     long_description_content_type="text/markdown",
